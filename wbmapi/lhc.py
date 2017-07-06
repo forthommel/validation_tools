432f11017000ad3436b76149b099248fc29422b3
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-07-06 12:43:46 +0200

    Cleanup of the lhc classes

--- wbmapi/lhc.py
+++ wbmapi/lhc.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib2
-import auth
-import xml.etree.ElementTree as tree
 from datetime import datetime
 
 class LHCstatus:
@@ -41,29 +38,4 @@ class LHCFillScheme:
         return int(self._scheme_[4])
     def variant(self):
         return self._scheme_[5]
-
-class api:
-    _base_url_ = 'https://cmswbm.cern.ch/cmsdb/servlet'
-
-    def __init__(self):
-        print 'api created... base url=%s' % (self._base_url_)
-
-    def getLHCstatus(self):
-        data = auth.getContent('%s/LhcMonitor?FORMAT=XML' % self._base_url_)
-        data = tree.fromstring(data)
-
-        for s in data.findall('LhcSample'):
-            lhc = LHCstatus()
-
-            try:
-                lhc.fillnum = int(s.find('FILLN').text)
-                print s.find('collectionTimeGMT').text, datetime.strptime(s.find('collectionTimeGMT').text+' GMT', '%Y.%m.%d %H:%M:%S %Z')
-                lhc.lastupdate = datetime.strptime(s.find('collectionTimeGMT').text+' GMT', '%Y.%m.%d %H:%M:%S %Z')
-                lhc.collidingbunches = int(s.find('nCollidingBunches').text)
-                lhc.injectionsch = LHCFillScheme(s.find('activeInjectionScheme').text.strip())
-                lhc.comment = s.find('lhcPageOne').text.strip()
-                lhc.betastar5 = float(s.find('BSTAR5').text)
-            except AttributeError: pass
-
-            return lhc
 
